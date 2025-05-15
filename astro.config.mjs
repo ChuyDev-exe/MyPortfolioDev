073bf69b11b6eecdd46d9ce7a441a6a1ecb61bd6
JesusMac <afranvlez@gmail.com>
2025-05-15 11:46:02 -0600

    node implementation for Tenant

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()]
+  integrations: [tailwind(), react(), mdx()],
+    adapter: node({ mode: 'standalone' }),
 });
